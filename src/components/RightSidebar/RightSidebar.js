59cd2ddad9b34b051222bddc21f38bc6f30cfa52
Talibiq123 <talibsaeed90@gmail.com>
2024-09-21 21:17:44 +0530

    UI complete for each page...

--- src/components/RightSidebar/RightSidebar.js
+++ src/components/RightSidebar/RightSidebar.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './RightSidebar.css'
+import assets from '../../assets/assets'
 
 const RightSidebar = () => {
   return (
-    <div>
-      
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="profile-img" />
+        <h3>Richard Sanford <img src={assets.green_dot} className='dot' alt="online" /></h3>
+        <p>Hey there, I am Richard Stanford using chat app.</p>
+      </div>
+      <hr />
+      <div className='rs-media'>
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="sharing-pic" />
+          <img src={assets.pic2} alt="sharing-pic" />
+          <img src={assets.pic3} alt="sharing-pic" />
+          <img src={assets.pic4} alt="sharing-pic" />
+          <img src={assets.pic1} alt="sharing-pic" />
+          <img src={assets.pic2} alt="sharing-pic" />
+        </div>
+      </div>
+      <button>Logout</button>
     </div>
   )
 }
