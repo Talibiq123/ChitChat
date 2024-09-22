aa4bdebeb9b85eeb36c9705b0210e502647420d5
Talibiq123 <talibsaeed90@gmail.com>
2024-09-22 19:21:59 +0530

    logout done...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Login from '../src/pages/Login/Login'
 import Chat from '../src/pages/Chat/Chat'
 import ProfileUpdate from '../src/pages/ProfileUpdate/ProfileUpdate'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './config/firebase';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate('/chat')
+      } else {
+        navigate('/')
+      }
+    })
+  }, [])
+
   return (
     <>
       <ToastContainer />
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 import assets from "../../assets/assets";
-import { signup } from "../../config/firebase";
+import { signup, login } from "../../config/firebase";
 
 const Login = () => {
   const [currState, setCurrState] = useState("Sign Up");
@@ -14,6 +14,8 @@ const Login = () => {
     console.log("Form Submitted", { userName, email, password });
     if (currState === "Sign Up") {
       signup(userName, email, password);
+    } else {
+      login(email, password);
     }
   }  
 
