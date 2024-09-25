a77dc16b00ebcf249384e0c26ec600728ccc37f3
Talibiq123 <talibsaeed90@gmail.com>
2024-09-25 18:16:58 +0530

    Profile Update...

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -11,29 +11,79 @@ const upload = async (file) => {
 
   const uploadTask = uploadBytesResumable(storageRef, file);
 
-  uploadTask.on(
-    "state_changed",
-    (snapshot) => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-      switch (snapshot.state) {
-        case "paused":
-          console.log("Upload is paused");
-          break;
-        case "running":
-          console.log("Upload is running");
-          break;
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // return (downloadURL)
+          resolve(downloadURL)
+        });
       }
-    },
-    (error) => {
-      
-    },
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        resolve(downloadURL)
-      });
-    }
-  );
-};
+    );
+  })
+}
 
 export default upload
+
+
+// import {
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+//   getDownloadURL,
+// } from "firebase/storage";
+
+// const uploadFileToFirebase = async (file) => {
+//   const storage = getStorage();
+//   const storageRef = ref(storage, `images/${Date.now()}_${file.name}`);
+
+//   const uploadTask = uploadBytesResumable(storageRef, file);
+
+//   return new Promise((resolve, reject) => {
+//     uploadTask.on(
+//       "state_changed",
+//       (snapshot) => {
+//         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//         console.log("Upload is " + progress + "% done");
+//         switch (snapshot.state) {
+//           case "paused":
+//             console.log("Upload is paused");
+//             break;
+//           case "running":
+//             console.log("Upload is running");
+//             break;
+//         }
+//       },
+//       (error) => {
+//         console.error("Upload failed:", error);
+//         reject(error);  // Reject the promise on error
+//       },
+//       async () => {
+//         try {
+//           const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+//           resolve(downloadURL);  // Resolve the promise with the download URL
+//         } catch (error) {
+//           reject(error);  // Handle any errors in getting the download URL
+//         }
+//       }
+//     );
+//   });
+// };
+
+// export default uploadFileToFirebase;
--- src/pages/ProfileUpdate/ProfileUpdate.js
+++ src/pages/ProfileUpdate/ProfileUpdate.js
@@ -1,22 +1,93 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProfileUpdate.css'
+import { onAuthStateChanged } from 'firebase/auth'
+import {auth, db} from '../../config/firebase'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import assets from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import upload from '../../lib/upload'
 
 const ProfileUpdate = () => {
+
+  const navigate = useNavigate();
+
   const [image, setImage] = useState(false);
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+
+  const profileUpdate = async (event) => {
+
+    event.preventDefault();
+
+    try {
+
+      if ( !prevImage && !image ) {
+        toast.error("Upload Profile Picture");
+      }
+      const docRef = doc(db, 'user', uid)
+      if (image) {
+        const imgURL = await upload(image);
+        setPrevImage(imgURL);
+        await updateDoc(docRef, {
+          avatar: imgURL,
+          bio: bio,
+          name: name
+        })
+      } else {
+        await updateDoc(docRef, {
+          bio: bio,
+          name: name
+        })
+      }
+
+    } catch (error) {
+      
+    }
+
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid)
+        const docRef = doc(db, "user", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.data().name) {
+          setName(docSnap.data().name)
+        }
+
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio)
+        }
+
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar)
+        }
+
+      } else {
+        navigate('/');
+      }
+
+    })
+
+  }, [])
 
   return (
     <div className='profile'>
       <div className='profile-container'>
-        <form>
+        <form onSubmit={profileUpdate}>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id='avatar' accept=' .png, .jpg, .jpeg' hidden />
             <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="avatar" />
             upload profile image
           </label>
-          <input type="text" placeholder='your name' required />
-          <textarea placeholder='Wirte profile bio' required></textarea>
+          <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder='your name' required />
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder='Wirte profile bio' required></textarea>
           <button type='submit'>Save</button>
         </form>
         <img className='profile-pic' src={image? URL.createObjectURL(image): assets.logo_icon} alt="logo" />
