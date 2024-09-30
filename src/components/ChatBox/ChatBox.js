9a84ff49dfb4703aaccd415f77d5fddcda64c040
Talibiq123 <talibsaeed90@gmail.com>
2024-09-30 15:35:22 +0530

    chatting...

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -1,13 +1,64 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './ChatBox.css'
 import assets from '../../assets/assets'
 import { AppContext } from '../../context/AppContext'
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore'
+import { db } from '../../config/firebase'
+import { toast } from 'react-toastify'
 
 const ChatBox = () => {
 
   const {userData, messageId, chatUser, messages, setMessages} = useContext(AppContext);
 
   const [input, setInput] = useState("");
+
+  const sendMessage = async () => {
+    try {
+      if (input && messageId) {
+        await updateDoc((db, 'messages', messageId),{
+          messages: arrayUnion({
+            sId: userData.id,
+            text: input,
+            createdAt: new Date(),
+          })
+        })
+
+        const userIds = [chatUser.rId, userData.id];
+
+        userIds.forEach(async (id) => {
+          const userChatsRef = doc(db, 'chats', id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex((c) => c.messageId === messageId);
+            userChatData.chatsData[chatIndex].lastMessage = input.slice(0, 30);
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatsRef, {
+              chatsData: userChatData.chatsData
+            })
+          }
+        })
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setInput("");
+  }
+
+  useEffect((req, res) => {
+    if (messageId) {
+      const unSub = onSnapshot(doc(db, "messages", messageId), () => {
+        setMessages(res.data().messages.reverse());
+      })
+      return () => {
+        unSub();
+      }
+    }
+  }, messageId)
 
   return chatUser?  (
     <div className='chat-box'>
@@ -18,13 +69,19 @@ const ChatBox = () => {
       </div>
 
       <div className='chat-msg'>
-        <div className="s-msg">
-          <p className='msg'>Lorem Ipsum is placeholder text commonly used in ...</p>
-          <div>
-            <img src={assets.profile_img} alt="profile pic" />
-            <p>03:34 PM</p>
-          </div>
-        </div>
+        {
+          messages.map((msg, index) => (
+            <div className="s-msg">
+            <p className='msg'>Lorem Ipsum is placeholder text commonly used in ...</p>
+            <div>
+              <img src={assets.profile_img} alt="profile pic" />
+              <p>03:34 PM</p>
+            </div>
+            </div>
+          ))
+        }
+
+        
         <div className="s-msg">
           <img className='msg-img' src={assets.pic1} alt="pic1" />
           <div>
@@ -42,12 +99,12 @@ const ChatBox = () => {
       </div>
 
       <div className='chat-input'>
-        <input type="text" placeholder='Send a message' />
+        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Send a message' />
         <input type="file" id='img' accept='image/png, image/jpeg' hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="gallery icon" />
         </label>
-        <img src={assets.send_button} alt="send button" />
+        <img onClick={sendMessage} src={assets.send_button} alt="send button" />
       </div>
     </div>
   ) 
